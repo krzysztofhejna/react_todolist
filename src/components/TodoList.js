50bf1d70798e36748fa78481788fa38099db9a73
krzysztofhejna <khejna15@wp.pl>
2019-02-13 11:29:09 +0100

    Implement HMR into the app

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import style from './TodoList.css';
+import Todo from './Todo';
 
 const TodoList = props => {
   const tasks = props.data.map((task) =>
-    <li key={task.id}>
-      {task.text}
-      <button onClick={() => props.taskRemove(task.id)}>Remove this task</button>
-    </li>
+    <Todo 
+      key={task.id}
+      text={task.text}
+      onClick={() => props.taskRemove(task.id)}
+    />
   );
 
   return (
     <ul className={style.TodoList}>
       {tasks}
     </ul>
-  );
+  )
 }
 
 export default TodoList;
